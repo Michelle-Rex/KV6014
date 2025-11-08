875ce58fd71a466213f4c7c882de51532da629c9
Michelle Rex <162601741+Michelle-Rex@users.noreply.github.com>
2025-11-08 10:53:50 +0000

    Added markComplete and addTask functions

--- pages/CarerToDoList.py
+++ pages/CarerToDoList.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import datetime
 
 class Task:
     def __init__(self, task):
@@ -8,4 +9,15 @@ class Task:
         self.date_complete = ""
         self.time_complete = ""
 
+    def mark_complete(self):
+        self.isComplete = True
+        self.date_complete = datetime.datetime.now().strftime("%x")
+        self.time_complete = datetime.datetime.now().strftime("%X")
+
+class ToDoList:
+    def __init__(self):
+        self.taskList = []
+
+    def addTask(self, task: Task):
+        self.taskList.append(task)
 
