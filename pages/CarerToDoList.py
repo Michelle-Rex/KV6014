2899326db2f76384df1c2b68b6fd539d15246968
Michelle Rex <162601741+Michelle-Rex@users.noreply.github.com>
2025-11-08 11:37:46 +0000

    Make getters and setters for Task class

--- pages/CarerToDoList.py
+++ pages/CarerToDoList.py
@@ -2,22 +2,41 @@ import streamlit as st
 import datetime
 
 class Task:
-    def __init__(self, task):
+    def __init__(self, task): #constructor
         self.task = task
-        self.task_ID = ""
+        self.task_id = ""
         self.isComplete = False
         self.date_complete = ""
         self.time_complete = ""
 
-    def mark_complete(self):
+    #getters and setters
+    def getTask(self): 
+        return self.task
+    
+    def setTask(self, task):
+        self.task = task
+
+    def getID(self):
+        return self.task_id
+
+    def markComplete(self): #mark task as complete and get the date and time of completion
         self.isComplete = True
         self.date_complete = datetime.datetime.now().strftime("%x")
         self.time_complete = datetime.datetime.now().strftime("%X")
+    
+    def markIncomplete(self): #reset the task competion
+        if self.isComplete == True:
+            self.isComplete = False
+            self.date_complete = ""
+            self.time_complete = ""
+
 
 class ToDoList:
-    def __init__(self):
+    def __init__(self): #make the list, should trigger when carer selects a patient, should run something to get the existing list from the database
         self.taskList = []
 
-    def addTask(self, task: Task):
+    def addTask(self, task: Task): #add task to list
         self.taskList.append(task)
+
+    
 
