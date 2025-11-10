19bb4ce05dbba6975ab7147b695224e88f1d0808
DinosaurusRexy <162601741+DinosaurusRexy@users.noreply.github.com>
2025-11-10 10:24:47 +0000

    Making the to do list interface

--- pages/CarerToDoList.py
+++ pages/CarerToDoList.py
@@ -1,3 +1,18 @@
 import streamlit as st
 
-st.text("hello")
+def main():
+    st.title("To Do List")
+
+    st.button("Create New Task", type = "secondary", on_click=makeTaskMenu())
+
+
+def makeTaskMenu():
+    st.subheader("New Task")
+    col1 = st.beta_columns(2)
+
+    with col1:
+        task = st.text_area("Task")
+        
+
+    if st.button("Add To List"):
+        st.success("Task Added")
