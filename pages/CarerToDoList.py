0c38e272f9a5ec462214eae0030a52e65119d36d
DinosaurusRexy <162601741+DinosaurusRexy@users.noreply.github.com>
2025-11-12 14:32:27 +0000

    Added an input for due date to To Do List

--- pages/CarerToDoList.py
+++ pages/CarerToDoList.py
@@ -8,11 +8,14 @@ def main():
 
 def makeNewTask():
     st.subheader("New Task")
-    col1 = st.beta_columns(2)
+    col1, col2 = st.beta_columns(2)
 
     with col1:
         task = st.text_area("Task")        
 
+    with col2:
+        task_due_date = st.date_input("Due Date")
+        
     if st.button("Add To List"):
         st.success("Task Added")
 
